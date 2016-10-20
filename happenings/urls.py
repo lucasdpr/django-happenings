0361cf42ec614ee97d3835d26db19b89a7227e09
sachinks <sachin.kolige@gmail.com>
2016-10-20 21:11:03 +0530

    Updated happenings/urls.py to make it work with django 1.10
    Updated happenings/urls.py to make it work with django 1.10

--- happenings/urls.py
+++ happenings/urls.py
@@ -1,11 +1,10 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from . import views
 
 
-urlpatterns = patterns('',
-    url(r'^$', views.EventMonthView.as_view(), name='list'),
+urlpatterns = url(r'^$', views.EventMonthView.as_view(), name='list'),
     url(r'^month/shift/$', views.EventMonthView.as_view(), name='month_shift'),
     url(r'^event-list/shift/$', views.EventMonthView.as_view(), name='event_list_shift'),
     url(r'^cal-and-list/shift/$', views.EventMonthView.as_view(), name='cal_and_list_shift'),
